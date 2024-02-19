e332b24bbff46f370cd09d865bee248b6d0f92e4
ahmd-fariz <zearavenclaww@gmail.com>
2024-02-19 10:33:08 +0700

    menambahkan folder

---
+++ Jeni_XI RPL1/about.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="style0.css">
+
+</head>
+<body>
+   
+
+<div class="heading">
+   <h3>about us</h3>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="img/produk3.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>Di toko kami juga menyediakan berbagai macam koleksi buku secara online pelanggan bisa dengan mudah untuk mengunjungi toko kami ini.kamu juga bisa dengan detail untuk mencari buku-buku pada search barnya</p>
+         <p></p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">client's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="img/produk10.jpg" alt="">
+         <p>ini bagus banget harga murah kualitas nya juga bagus next bakal order di sini lagi </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Zyan</h3>
+      </div>
+
+      <div class="box">
+         <img src="img/produk7.jpg" alt="">
+         <p>Dari judul nya sih menarik untuk dibaca dan ternyata dari daftar isi nya sangat menarik,thanks seller</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Zeva</h3>
+      </div>
+
+      <div class="box">
+         <img src="img/produk3.jpg" alt="">
+         <p>bagus buku nya anak saya suka sama buku-buku di toko ini</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Aslan</h3>
+      </div>
+
+      <div class="box">
+         <img src="img/produk5.jpg" alt="">
+         <p>novel di sini bagus suka sama judul-judul nya harga nya juga terjangkau</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Aily</h3>
+      </div>
+
+      <div class="box">
+         <img src="img/produk6.jpg" alt="">
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Gibran</h3>
+      </div>
+
+      <div class="box">
+         <img src="img/produk8.jpg" alt="">
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ad, quo labore fugiat nam accusamus quia. Ducimus repudiandae dolore placeat.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Kanaya</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="authors">
+
+   <h1 class="title">greate authors</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="img/pink.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Raka</h3>
+      </div>
+
+      <div class="box">
+         <img src="img/produk2.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Clairine</h3>
+      </div>
+
+      <div class="box">
+         <img src="img/produk4.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Jeffran</h3>
+      </div>
+
+      <div class="box">
+         <img src="img/produk5.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Zevanya</h3>
+      </div>
+
+      <div class="box">
+         <img src="img/produk5.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Nicholl</h3>
+      </div>
+
+      <div class="box">
+         <img src="img/produk5.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Jovanka</h3>
+      </div>
+
+   </div>
+
+</section>
+
+</body>
+</html>
